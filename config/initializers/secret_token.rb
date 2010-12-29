a7b9c6080f27857ed474bbee037cad67e7a2c5d6
wrampix <wrampix@gmail.com>
2010-12-29 14:37:39 +0100

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Przelnik::Application.config.secret_token = '016f259fa01bf47db31ea2922ceaceab2e75013a4e4915bf1eadf47508907537c0140157a0f0b1285d425f971f8bee020bbb5defe87af8ea8f25f7312a042642'
